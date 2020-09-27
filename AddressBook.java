90acec812370e443e0ea48b73f486909ed8ecb5a
Jay002 <jayesh2598@gmail.com>
2020-09-28 01:17:55 +0530

    Added: Feature for adding new contacts using loop added

---
+++ AddressBook.java
@@ -0,0 +1,21 @@
+package com.capgemini.training;
+
+import java.util.*;
+public class AddressBook {
+	private List<Contact> addressBook = new ArrayList<Contact>();
+		
+	//Getters
+	public List<Contact> getAddressBook() {
+		return addressBook;
+	}
+	
+	//Setters
+	public void setAddressBook(List<Contact> addressBook) {
+		this.addressBook = addressBook;
+	}
+	
+	//Methods
+	public void addContact(Contact contact) {
+		addressBook.add(contact);
+	}
+}
