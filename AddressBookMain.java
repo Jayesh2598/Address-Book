a0e096e5f05670b5ebdab1a7fc6287440af1c003
Jay002 <jayesh2598@gmail.com>
2020-09-28 00:35:31 +0530

    Start: Welcome message is displayed

---
+++ AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.capgemini.training;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome to Address Book Program!");
+		
+	}
+}
