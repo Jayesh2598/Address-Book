897206f063875517d7d76c47e976f937659f89f0
Jay002 <jayesh2598@gmail.com>
2020-09-29 21:23:55 +0530

    Added: Feature to add multiple contacts in same addressbook added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,18 +3,50 @@ package com.capgemini.training;
 import java.util.*;
 
 public class AddressBookMain {
-
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Welcome to Address Book Program!");
+		
+		AddressBookDictionary dictionary = new AddressBookDictionary();
+		boolean makeNewAddressBook=true;
+		while(makeNewAddressBook) {
+			System.out.println("Enter your choice:\n1. Create new Address Book\t2. Access existing Address Book\t3. Exit");
+			int choice1=Integer.parseInt(sc.nextLine());
 
-		AddressBook book= new AddressBook();
+			switch(choice1) {
+			case 1:
+				System.out.println("Enter the Address Book's name:");
+				String newBookName=sc.nextLine();
+				AddressBook newBook= new AddressBook();
+				editAddressBook(newBook);
+				dictionary.addAddressBook(newBookName, newBook);
+				break;
+			
+			case 2:
+				System.out.println("Enter the Address Book's name:");
+				String accessBookName=sc.nextLine();
+				AddressBook accessBook= dictionary.accessAddressBook(accessBookName);
+				editAddressBook(accessBook);
+				dictionary.addAddressBook(accessBookName,accessBook);
+				break;
+			case 3:
+				System.out.println("Thank you for using the application.");
+				makeNewAddressBook=false;
+				break;
+			}	
+		}
+		sc.close();
+	}
+	
+	public static void editAddressBook (AddressBook book) {
 		boolean loop=true;
 		while(loop) {
-			System.out.println("Enter your choice:\n1. Enter a new contact\n2. Edit an existing contact\n3. Delete an existing contact\n4. Exit");
-			int choice= Integer.parseInt(sc.nextLine());
+			System.out.println("Enter your choice:\n1. Enter a new contact\n2. Edit an existing contact\n3. Delete an existing contact\n4. Exit from this address book");
+			int choice2= Integer.parseInt(sc.nextLine());
 			
-			switch(choice) {
+			switch(choice2) {
 			case 1:
 				System.out.println("Enter the first name:");
 				String firstName = sc.nextLine().trim();
@@ -53,11 +85,10 @@ public class AddressBookMain {
 				book.deleteContact(FName, LName);
 				break;
 			case 4:
-				System.out.println("Thank you for using the application.");
+				System.out.println("Exiting from this address book.");
 				loop=false;
 				break;
-			}	
+			}
 		}
-		sc.close();
 	}
 }
