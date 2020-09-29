22d0521587937e9473a73e5790c530784bc8738c
Jay002 <jayesh2598@gmail.com>
2020-09-29 21:27:22 +0530

    Refactored: Multiple address books can now be added in a dictionary

---
+++ AddressBookDictionary.java
@@ -0,0 +1,28 @@
+package com.capgemini.training;
+
+import java.util.HashMap;
+
+public class AddressBookDictionary {
+	
+	private HashMap<String,AddressBook> dictionary = new HashMap<>();
+
+	//Getter
+	public HashMap<String, AddressBook> getDictionary() {
+		return dictionary;
+	}
+	
+	//Setter
+	public void setDictionary(HashMap<String, AddressBook> dictionary) {
+		this.dictionary = dictionary;
+	}
+	
+	//Methods
+	public void addAddressBook(String bookName,AddressBook book) {
+		dictionary.put(bookName, book);
+	}
+	public AddressBook accessAddressBook(String bookName) {
+		AddressBook book= dictionary.get(bookName);
+		dictionary.remove(bookName);
+		return book;
+	}
+}
